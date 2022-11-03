fb03f000446464f943247b33c27d58278bbde8b1
Takumi Ohtake <s20c1025ng@s.chibakoudai.jp>
2022-11-03 12:53:40 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -16,7 +16,7 @@ out=$(seq 5 | ./plus)
 ### STRANGE INPUT ###
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = ""]  || ng ${LINENO}
+[ "${out}" = "" ]  || ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
